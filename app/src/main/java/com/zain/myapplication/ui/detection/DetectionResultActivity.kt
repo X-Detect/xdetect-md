52bca724386954e12093abca63f5564756c90038
zainatul31 <104212527+zainatul31@users.noreply.github.com>
2023-05-25 23:16:33 +0700

    up logo, splash

---
+++ app/src/main/java/com/zain/myapplication/ui/detection/DetectionResultActivity.kt
@@ -0,0 +1,12 @@
+package com.zain.myapplication.ui.detection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zain.myapplication.R
+
+class DetectionResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detection_result)
+    }
+}
